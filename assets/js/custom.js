9a81c68370228a9e772df1f5467277a7410c2562
Neil Boyd <neilboyd@gmail.com>
2024-03-13 18:34:02 +0100

    style audio button when playing

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,24 @@
 function playAudio(el) {
-  el.getElementsByTagName('audio')[0].play()
+  const audio = el.getElementsByTagName('audio')[0];
+  audio.addEventListener("play", () => startPlayAudio(el));
+  audio.addEventListener("ended", () => endPlayAudio(el));
+  audio.onerror = () => errorPlayAudio(el);
+  audio.play()
+}
+
+function startPlayAudio(el) {
+  el.classList.remove("fa-volume-low")
+  el.classList.add("fa-volume-high");
+  el.classList.add("text-info");
+}
+
+function endPlayAudio(el) {
+  el.classList.remove("fa-volume-high")
+  el.classList.remove("text-info");
+  el.classList.add("fa-volume-low");
+}
+
+function errorPlayAudio(el) {
+  el.classList.add("text-danger");
+  endPlayAudio(el);
 }
