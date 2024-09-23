0017894e35bc4476942298de6108a5ce7d0920d2
Neil Boyd <NeilBoyd@gmail.com>
2024-09-23 05:52:27 +0000

    handle events in document handler

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,25 +1,36 @@
 window.keyListener = function (e) {
-  console.log('key listener', e)
+  console.log('key listener', e);
   switch (e.code) {
     case 'ArrowLeft':
-      console.log('left');
-      e.preventDefault()
+      e.preventDefault();
+      for (const audio of document.getElementsByTagName('audio')) {
+        audio.currentTime = Math.max(0, audio.currentTime - 10);
+      }
       break;
     case 'ArrowRight':
-      console.log('right');
-      e.preventDefault()
+      e.preventDefault();
+      for (const audio of document.getElementsByTagName('audio')) {
+        if (audio.currentTime) {
+          audio.currentTime = Math.min(audio.duration, audio.currentTime + 10);
+        }
+      }
       break;
     case 'Space':
-      console.log('space');
-      e.preventDefault()
-      window.dispatchEvent(new Event('foo'))
+      e.preventDefault();
+      for (const audio of document.getElementsByTagName('audio')) {
+        if (audio.currentTime) {
+          if (audio.paused) {
+            audio.play();
+          } else {
+            audio.pause();
+          }
+        }
+      }
       break;
-  }  
-}
+  }
+};
 
-window.addEventListener('keydown', window.keyListener)
-
-window.addEventListener('foo', () => console.log('window foo'))
+window.addEventListener('keydown', window.keyListener);
 
 const playAudio = function (el) {
   const audio = el.getElementsByTagName('audio')[0];
@@ -30,42 +41,38 @@ const playAudio = function (el) {
       audio.pause();
     }
   } else {
-    audio.addEventListener("play", () => startPlayAudio(el));
-    audio.addEventListener("pause", () => pauseAudio(el));
-    audio.addEventListener("ended", () => endPlayAudio(el));
-
-    audio.addEventListener('foo', () => console.log('audio foo'))
-    window.addEventListener('foo', () => console.log('window audio foo'))
-
+    audio.addEventListener('play', () => startPlayAudio(el));
+    audio.addEventListener('pause', () => pauseAudio(el));
+    audio.addEventListener('ended', () => endPlayAudio(el));
     audio.onerror = () => errorPlayAudio(el);
     audio.play();
   }
-}
+};
 
 const startPlayAudio = function (el) {
-  el.classList.remove("fa-volume-low")
-  el.classList.remove("fa-pause")
-  el.classList.add("fa-volume-high");
-  el.classList.add("text-info");
-}
+  el.classList.remove('fa-volume-low');
+  el.classList.remove('fa-pause');
+  el.classList.add('fa-volume-high');
+  el.classList.add('text-info');
+};
 
 const pauseAudio = function (el) {
-  console.log("pause")
-  el.classList.remove("fa-volume-high")
-  el.classList.add("fa-pause");
-}
+  console.log('pause');
+  el.classList.remove('fa-volume-high');
+  el.classList.add('fa-pause');
+};
 
 const endPlayAudio = function (el) {
-  console.log("end")
-  el.classList.remove("fa-volume-high")
-  el.classList.remove("fa-pause")
-  el.classList.remove("text-info");
-  el.classList.add("fa-volume-low");
-}
+  console.log('end');
+  el.classList.remove('fa-volume-high');
+  el.classList.remove('fa-pause');
+  el.classList.remove('text-info');
+  el.classList.add('fa-volume-low');
+};
 
 const errorPlayAudio = function (el) {
-  el.classList.remove("fa-volume-high")
-  el.classList.remove("text-info");
-  el.classList.add("fa-volume-xmark");
-  el.classList.add("text-danger");
-}
+  el.classList.remove('fa-volume-high');
+  el.classList.remove('text-info');
+  el.classList.add('fa-volume-xmark');
+  el.classList.add('text-danger');
+};
